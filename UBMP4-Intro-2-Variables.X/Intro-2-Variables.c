2bc2edb11ab5536ba0ca710410d04eef6dd388c7
da640190@wellingtoncdsb.ca <da640190@wellingtoncdsb.ca>
2022-04-21 22:58:04 +0000

    activity 1-4

--- UBMP4-Intro-2-Variables.X/Intro-2-Variables.c
+++ UBMP4-Intro-2-Variables.X/Intro-2-Variables.c
@@ -23,7 +23,7 @@
 
 #define pressed 0
 #define notPressed 1
-#define A4
+#define A1
 #ifdef ProgramAnalysis
 // Program constant definitions
 const unsigned char maxCount = 5;
@@ -97,7 +97,6 @@ unsigned char SW2Count = 0;
 bool SW2Pressed = false;
 unsigned char SW5Count = 0;
 bool SW5Pressed = false;
-
 
 int main(void)
 {
@@ -181,15 +180,12 @@ int main(void)
 }
 #endif
 #ifdef A2
-<<<<<<< HEAD
 // Toggle button that turns an LED on when pressed, then off when pressed again.
 // Variables
 bool SW2Pressed = false;
-=======
 //Toggle button that turns an LED on when pressed, then off when pressed again.
 //Variables
 bool LED3light = false;
->>>>>>> ff2d331c2a47e832eb74225c248e2b58bd57ab90
 
 int main(void)
 {
@@ -230,7 +226,6 @@ int main(void)
     {
         if(SW2 == pressed)
         {
-<<<<<<< HEAD
             LED3 = 1;
             SW2Count += 1;
         }
@@ -247,11 +242,6 @@ int main(void)
             SW2Count = 0;
         }
 
-=======
-
-        }
-
->>>>>>> ff2d331c2a47e832eb74225c248e2b58bd57ab90
         __delay_ms(10);
 
         if(SW1 == pressed)
@@ -261,16 +251,13 @@ int main(void)
     }
 }
 #endif
-<<<<<<< HEAD
 #ifdef A4
 // A program that tests for switch bounce. Press SW2 and if LED4 lights there is switch bounce.
 // Variables
 const char timeCount = 2;
 unsigned char SW2Count = 0;
 bool SW2Pressed = false;
-=======
-#ifdef A3
->>>>>>> ff2d331c2a47e832eb74225c248e2b58bd57ab90
+
 int main(void)
 {
     // Configure oscillator and I/O ports. These functions run once at start-up.
@@ -278,7 +265,7 @@ int main(void)
     UBMP4_config();             // Configure on-board UBMP4 I/O devices
     while(1)
     {
-<<<<<<< HEAD
+
     // Count new SW2 button presses
        if(SW2 == pressed && SW2Pressed == false)
        {
@@ -312,23 +299,7 @@ int main(void)
             LED4 = 0;
             SW2Count = 0;
         }
-=======
-        if(SW1 == pressed)
-        {
-            RESET();
-        }
-    }
-}
-#endif
-#ifdef A4
-int main(void)
-{
-    // Configure oscillator and I/O ports. These functions run once at start-up.
-    OSC_config();               // Configure internal oscillator for 48 MHz
-    UBMP4_config();             // Configure on-board UBMP4 I/O devices
-    while(1)
-    {
->>>>>>> ff2d331c2a47e832eb74225c248e2b58bd57ab90
+
         if(SW1 == pressed)
         {
             RESET();
